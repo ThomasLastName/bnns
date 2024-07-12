d91febff47461bdee1dc316f58e873721cdc7c2a
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-12 14:43:25 -0600

    Update demo.py

--- scripts/demo.py
+++ scripts/demo.py
@@ -11,11 +11,13 @@ except:
             path = os.getcwd()
             context, dirname = os.path.split(path)
             while not dirname=="bnns":
+                print("path")
                 path = context
                 context, dirname = os.path.split(path)
                 if len(dirname)==0:
                     raise Exception(f"bnns is not an ancestor of {os.getcwd()}")
             sys.path.append(path)
+            print(f"adding {path} to the PATH")
             from bnns.SSGE import func
             print("hacked import worked")
 
