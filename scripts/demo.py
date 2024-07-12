088d6a4e9b26489403abdee703079334f353d8f4
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-12 15:44:43 -0600

    Update demo.py

--- scripts/demo.py
+++ scripts/demo.py
@@ -3,20 +3,19 @@ try:
     print("absolute import worked")
 except:
     #
-    # # ~~~ Add the root directory of the repo to sys.path
+    # ~~~ Add the root directory of the repo to sys.path
     import sys
-    from quality_of_life.my_base_utils import find_root_dir_of_repo
-    sys.path.append(find_root_dir_of_repo())
-    # import os
-    # import sys
-    # path = os.getcwd()
-    # while not os.path.exists(os.path.join(path,".git")):
-    #     path, dirname = os.path.split(path)
-    #     if len(dirname)==0:
-    #         raise OSError(f"The stopping criterion was not met by any ancestor of the working directory {os.getcwd()}")
-    # sys.path.append(path)
-    # #
-    # # ~~~ Now import
+    # from quality_of_life.my_base_utils import find_root_dir_of_repo
+    # sys.path.append(find_root_dir_of_repo())
+    import os
+    path = os.getcwd()
+    while not os.path.exists(os.path.join(path,".git")):
+        path, dirname = os.path.split(path)
+        if len(dirname)==0:
+            raise OSError(f"The stopping criterion was not met by any ancestor of the working directory {os.getcwd()}")
+    sys.path.append(path)
+    #
+    # ~~~ Now import
     from bnns.SSGE import func
 
 func()
