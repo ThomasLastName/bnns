4a3cfb9dd54306170340ce81ae629189b057c44b
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-12 14:36:48 -0600

    Update demo.py

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,12 +1,18 @@
-import os
-import sys
-path = os.getcwd()
-context, dirname = os.path.split(path)
-while not dirname=="bnns":
-    path = context
-    context, dirname = os.path.split(path)
-    if len(dirname)==0:
-        raise Exception(f"bnns is not an ancestor of {os.getcwd()}")
-sys.path.append(path)
+try:
+    from ..bnns.SSGE import func
+except:
+    try:
+        from bnns import func:
+    except:
+            import os
+            import sys
+            path = os.getcwd()
+            context, dirname = os.path.split(path)
+            while not dirname=="bnns":
+                path = context
+                context, dirname = os.path.split(path)
+                if len(dirname)==0:
+                    raise Exception(f"bnns is not an ancestor of {os.getcwd()}")
+            sys.path.append(path)
 
 func()
