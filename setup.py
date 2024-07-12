edb48bdd4d75a67a5ae1539b4047be2789ee9e97
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-11 19:48:54 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+### ~~~
+## ~~~ From https://github.com/maet3608/minimal-setup-py/blob/master/setup.py
+### ~~~ 
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'bnns',
+    version = '1.2.0',
+    url = 'https://github.com/ThomasLastName/bnns.git',
+    author = 'Thomas Winckelman',
+    author_email = 'winckelman@tamu.edu',
+    description = 'Description of my package',
+    packages = find_packages(),    
+    install_requires = [],
+)
