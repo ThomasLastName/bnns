7af67fb93ec0fff1285379095525f73785169c83
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-12 16:26:26 -0600

    Create fixed_demo.py

---
+++ scripts/fixed_demo.py
@@ -0,0 +1,18 @@
+try:
+    1/0
+except:
+    #
+    # ~~~ Add the root directory of the repo to sys.path
+    import sys
+    import os
+    path = os.getcwd()
+    while not os.path.exists(os.path.join(path,".git")):
+        path, dirname = os.path.split(path)
+        if len(dirname)==0:
+            raise OSError(f"The stopping criterion was not met by any ancestor of the working directory {os.getcwd()}")
+    sys.path.append(path)
+    #
+    # ~~~ Now import
+    from bnns.SSGE import func
+
+func()
