3790224e28eb47ab9ea26011be7c7e2810ba07de
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-12 15:03:36 -0600

    Update demo.py

--- scripts/demo.py
+++ scripts/demo.py
@@ -6,13 +6,18 @@
 #         from bnns.SSGE import func
 #         print("absolute import worked")
 #     except:
+
 import os
 import sys
+folder_I_want_to_import_from = "bnns"
+
 path = os.getcwd()
 context, dirname = os.path.split(path)
-while not dirname=="bnns":
+while not dirname==folder_I_want_to_import_from:
     path = context
     context, dirname = os.path.split(path)
+    if len(dirname)==0:
+        raise OSError(f"The directory {folder_I_want_to_import_from} is not an ancestor of the working directory {os.getcwd()}")
 
 sys.path.append(path)
 from bnns.SSGE import func
