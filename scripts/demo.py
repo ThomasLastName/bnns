917cb2fc7360580d8e987924bb88a34951f476f3
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-12 15:30:24 -0600

    Update demo.py

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,25 +1,22 @@
-# try:
-#     from ..bnns.SSGE import func
-#     print("relative import worked")
-# except:
-#     try:
-#         from bnns.SSGE import func
-#         print("absolute import worked")
-#     except:
-
-import os
-import sys
-folder_I_want_to_import_from = "bnns"
-
-path = os.getcwd()
-context, dirname = os.path.split(path)
-while not dirname==folder_I_want_to_import_from:
-    path = context
-    context, dirname = os.path.split(path)
-    if len(dirname)==0:
-        raise OSError(f"The directory {folder_I_want_to_import_from} is not an ancestor of the working directory {os.getcwd()}")
-
-sys.path.append(path)
-from bnns.SSGE import func
+try:
+    from bnns.SSGE import func
+    print("absolute import worked")
+except:
+    #
+    # # ~~~ Add the root directory of the repo to sys.path
+    import sys
+    from quality_of_life.my_base_utils import find_root_dir_of_repo
+    sys.path.append(find_root_dir_of_repo())
+    # import os
+    # import sys
+    # path = os.getcwd()
+    # while not os.path.exists(os.path.join(path,".git")):
+    #     path, dirname = os.path.split(path)
+    #     if len(dirname)==0:
+    #         raise OSError(f"The stopping criterion was not met by any ancestor of the working directory {os.getcwd()}")
+    # sys.path.append(path)
+    # #
+    # # ~~~ Now import
+    from bnns.SSGE import func
 
 func()
