67642cf1217f6defa3113de753922402b209267b
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-12 14:59:09 -0600

    Update demo.py

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,24 +1,20 @@
-try:
-    from ..bnns.SSGE import func
-    print("relative import worked")
-except:
-    try:
-        from bnns.SSGE import func
-        print("absolute import worked")
-    except:
-            import os
-            import sys
-            path = os.getcwd()
-            context, dirname = os.path.split(path)
-            while not dirname=="bnns":
-                print("path")
-                path = context
-                context, dirname = os.path.split(path)
-                if len(dirname)==0:
-                    raise Exception(f"bnns is not an ancestor of {os.getcwd()}")
-            sys.path.insert(0,path)
-            print(f"adding {path} to the PATH")
-            from bnns.SSGE import func
-            print("hacked import worked")
+# try:
+#     from ..bnns.SSGE import func
+#     print("relative import worked")
+# except:
+#     try:
+#         from bnns.SSGE import func
+#         print("absolute import worked")
+#     except:
+import os
+import sys
+path = os.getcwd()
+context, dirname = os.path.split(path)
+while not dirname=="bnns":
+    path = context
+    context, dirname = os.path.split(path)
+
+sys.path.append(path)
+from bnns.SSGE import func
 
 func()
